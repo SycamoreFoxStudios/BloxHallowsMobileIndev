183e442267c1c5edffeeed81cf78bf635d9ed751
SycamoreFoxStudios <osborneadley@gmail.com>
2026-08-29 18:50:29 -0400

    Day 1

--- blox-hallows-mobile/blox-hallows-mobile/main_menu.gd
+++ blox-hallows-mobile/blox-hallows-mobile/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
-
-
+var MenuScreen = "Play"
+signal startgame
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("SplashScreen")
@@ -8,7 +8,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if MenuScreen == "Play":
+		$Camera2D.position = Vector2(640,360)
+	if MenuScreen == "Customize":
+		$Camera2D.position = Vector2(640,1200)
 
 
 func _on_button_pressed() -> void:
@@ -16,3 +19,17 @@ func _on_button_pressed() -> void:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	else:
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+
+func _on_play_button_down() -> void:
+	MenuScreen = "Play"
+
+
+func _on_customize_button_down() -> void:
+	MenuScreen = "Customize"
+
+
+func _on_start_game_button_down() -> void:
+	startgame.emit()
+	$AnimationPlayer.play("StartGame")
+	
